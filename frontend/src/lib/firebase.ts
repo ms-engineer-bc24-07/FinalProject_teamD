5d8255c7f044d0f37f0275455274e70b318d2159
rina toruson <linatuerxun@gmail.com>
2024-12-01 16:48:47 +0900

    fix.fairebaseの機密情報を.envファイルに移動

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCchMQcK2KtC3kvOOpmjD-iQcDsVlaq2HQ",
-  authDomain: "finalproject-teamd.firebaseapp.com",
-  projectId: "finalproject-teamd",
-  storageBucket: "finalproject-teamd.firebasestorage.app",
-  messagingSenderId: "725734820052",
-  appId: "1:725734820052:web:091580e050bc218c921341"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase

ff2158297b743aa5a4040af6660af5f123588d83
rina toruson <linatuerxun@gmail.com>
2024-12-04 11:07:32 +0900

    add.コンポーネント作成

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+// Footer.jsx
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-customBlue text-white p-4 mt-8">
+      <p className="text-customYellow text-sm">© 2024 My Website. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { FaUser } from 'react-icons/fa';// React Iconsからユーザーアイコンをインポート
+
+
+const Header = () => {
+  return (
+    <header className="bg-customBlue p-4 shadow-md flex items-center justify-between">
+       {/* 左側: タイトル */}
+      <h1 className="text-customYellow text-2xl font-bold">Tide Up</h1>
+      {/* 右側: マイページアイコン */}
+      <a
+        href="/mypage"
+        className="flex items-center text-white hover:text-gray-200 transition duration-200"
+        aria-label="Go to My Page"
+      >
+        <FaUser className="h-6 w-6" />
+        <span className="ml-2 text-sm">My Page</span>
+      </a>
+    </header>
+  );
+};
+
+export default Header;

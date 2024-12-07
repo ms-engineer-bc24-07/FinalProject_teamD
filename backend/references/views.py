8fc29669376e440553dd0e415d80b1caff6590b8
gami-g-2 <norika0127aaa@gmail.com>
2024-12-07 16:10:05 +0900

    update.backend

--- backend/references/views.py
+++ backend/references/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import ImageModel
+from .serializers import ImageModelSerializer
+
+class ImageUploadView(APIView):
+    def post(self, request, *args, **kwargs):
+        file_serializer = ImageModelSerializer(data=request.data)
+        if file_serializer.is_valid():
+            file_serializer.save()
+            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # Create your views here.

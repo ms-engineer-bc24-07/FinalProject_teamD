1741943993220d806d6c3c2b9e16534454cdb429
rina toruson <linatuerxun@gmail.com>
2024-12-14 20:00:50 +0900

    fix.URL設定のツイ生

--- backend/family/urls.py
+++ backend/family/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import send_invite, accept_invite
+from .views import send_invite, accept_invite, generate_invite_link
 
 urlpatterns = [
     path('send_invite/', send_invite, name='send_invite'),
     path('accept_invite/', accept_invite, name='accept_invite'),
+    path('generate_invite_link/', generate_invite_link, name='generate_invite_link'),  # 新しいエンドポイント
 ]

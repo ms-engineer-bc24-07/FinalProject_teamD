61a4cabb803c7e46ca1bd2499c1391937abd7199
rina toruson <linatuerxun@gmail.com>
2024-12-14 19:54:25 +0900

    fix.firebase-adminsdkのパスの修正

--- backend/firebase.py
+++ backend/firebase.py
@@ -1,8 +1,8 @@
 # 1. Firebase Admin SDKのセットアップ。Firebaseトークンを検証するには、Firebase Admin SDKを使用する。firebase_adminの初期化コードを作成。
 
 import firebase_admin
-from firebase_admin import credentials, auth
+from firebase_admin import credentials
 
 # Firebaseのサービスアカウントキー（JSONファイル）を設定
-cred = credentials.Certificate("/firebase-adminsdk.json")  # 適切なパスに変更
+cred = credentials.Certificate("./firebase-adminsdk.json")  # 適切なパスに変更
 firebase_admin.initialize_app(cred)

cb3c1e7e4d0a0eeff7c578db936b3dfaf67e8aac
rina toruson <linatuerxun@gmail.com>
2024-12-14 20:13:45 +0900

    fix.send_inviteを削除

--- backend/family/urls.py
+++ backend/family/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import send_invite, accept_invite, generate_invite_link
+from .views import  accept_invite, generate_invite_link
 
 urlpatterns = [
-    path('send_invite/', send_invite, name='send_invite'),
     path('accept_invite/', accept_invite, name='accept_invite'),
     path('generate_invite_link/', generate_invite_link, name='generate_invite_link'),  # 新しいエンドポイント
 ]

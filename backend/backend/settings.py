88b8a60625b5935887ae924b452b36f347e4288e
erinamatsunaga <f01erina590420@gmail.com>
2024-12-20 17:31:12 +0900

    add.loggerの設定

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -174,3 +174,21 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',  # デフォルト
     'users.authentication.FirebaseAuthenticationBackend',  # Firebase認証バックエンド
 ]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}

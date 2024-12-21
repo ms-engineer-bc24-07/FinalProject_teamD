c06580d810872e20e22b16744a6b7790a7d4f0aa
erinamatsunaga <f01erina590420@gmail.com>
2024-12-21 21:41:44 +0900

    fix.s3登録設定追加

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,17 @@
 import type { NextConfig } from "next";
 
-// 
-
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'teamd-finalproject.s3.ap-northeast-1.amazonaws.com',
-        pathname: '/images/**', // 画像のパスパターン
+        pathname: '/references/**', // 画像のパスパターンを references フォルダに合わせる
+      },
+      {
+        protocol: 'https',
+        hostname: 'teamd-finalproject.s3.ap-northeast-1.amazonaws.com',
+        pathname: '/comparison_images/**', // 比較用画像のパスパターン
       },
     ],
   },

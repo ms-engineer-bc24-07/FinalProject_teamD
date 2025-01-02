b5a48ddc5007c54ae44b6f45804d8b84015c4f59
rina toruson <linatuerxun@gmail.com>
2025-01-02 18:33:24 +0900

    add.fetch_referencesのパスを追加

--- backend/references/urls.py
+++ backend/references/urls.py
@@ -1,11 +1,12 @@
 # references/urls.py
 from django.urls import path
 from .views import ReferenceDetail
-from .views import ReferenceList
+# from .views import ReferenceList
 from .views import ReferenceView
+from .views import fetch_references
 
 urlpatterns = [
-    path('', ReferenceList.as_view(), name='reference-list'),
+    path('fetch_references/', fetch_references, name='fetch_references'),
     path('<int:reference_id>/', ReferenceDetail.as_view(), name='reference-detail'),  # 'api/references/<int:reference_id>/' に対応
     path('upload/', ReferenceView.as_view(), name="file-upload"),
 ]
